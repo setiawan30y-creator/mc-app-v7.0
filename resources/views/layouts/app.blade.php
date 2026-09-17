9265e78000ea62f1dbddf636c4381662068e2904
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-18 00:08:20 +0700

    Load transaction direction stabilizer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
     </main>
 </div>
 <script src="{{ asset('js/transaction-workspace.js') }}"></script>
+<script src="{{ asset('js/transaction-direction-stabilizer.js') }}"></script>
 @stack('scripts')
 </body>
 </html>
