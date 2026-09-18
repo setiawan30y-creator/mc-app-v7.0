cc5a9d3d4d1dcb6e5d080981e49b5c2b40a2e538
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-18 16:36:54 +0700

    fix: generate ULID for forex stock records

--- app/Models/ForexStock.php
+++ app/Models/ForexStock.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ForexStock extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
 
     protected $fillable = [
         'tenant_id',
